03a4b9a891cfa8e42c7aa78eee85f1c58abec0e8
phantamanta44 <evandalong@gmail.com>
2019-06-17 18:15:13 -0500

    Fix race conditions in page loading

--- assets/static/js/main.js
+++ assets/static/js/main.js
@@ -1,154 +1,170 @@
-(() => {
+window.onload = () => {
 
-    // sound
+  // sound
 
-    HTMLAudioElement.prototype.playRefreshing = function() {
-        this.pause();
-        this.currentTime = 0;
-        this.play();
+  HTMLAudioElement.prototype.playRefreshing = function() {
+    this.pause();
+    this.currentTime = 0;
+    this.play();
+  };
+
+  // globals
+
+  const elemWrapper = document.getElementById('wrapper');
+  const elemMain = document.getElementsByTagName('main')[0];
+  const resizeHandlers = [];
+
+  let rupeeCount = 0, displayedRupeeCount = 0;
+  let rupeeUpdateTask = null;
+
+  (storedRupeeCount => {
+    if (!isNaN(storedRupeeCount)) {
+      rupeeCount = displayedRupeeCount = Math.max(Math.min(storedRupeeCount, 999), 0);
+    }
+  })(parseInt(window.sessionStorage.getItem('rupees'), 10));
+
+  // navbar
+  const elemNav = document.getElementsByTagName('nav')[0];
+  const elemNavHl = document.getElementById('nav-highlight');
+
+  function genNavHlMutator(elem) {
+    return () => {
+      const parentBounds = elemNav.getBoundingClientRect();
+      const elemBounds = elem.getBoundingClientRect();
+      elemNavHl.style.width = `${elemBounds.width}px`;
+      elemNavHl.style.left = `${elemBounds.left - parentBounds.left}px`;
     };
+  }
 
-    // globals
+  const path = document.location.pathname;
+  const file = path === '/' ? 'index' : path.substring(path.lastIndexOf('/') + 1, path.lastIndexOf('.html'));
+  const elemNavSelected = document.getElementById(`nav-elem-${file}`);
+  const resetNavHl = genNavHlMutator(elemNavSelected);
+  elemNav.onmouseleave = resetNavHl;
+  for (const elem of document.getElementsByClassName('nav-elem')) {
+    elem.onmouseenter = genNavHlMutator(elem);
+  }
+  resizeHandlers.push(resetNavHl);
 
-    const elemWrapper = document.getElementById('wrapper');
-    const elemMain = document.getElementsByTagName('main')[0];
-    const resizeHandlers = [];
+  // mobile stylesheet application
+  const elemNavFirst = elemNav.firstElementChild;
+  const elemNavLast = elemNav.lastElementChild.previousElementSibling;
+  let resizeStyleTask = null;
 
-    let rupeeCount = 0, displayedRupeeCount = 0;
-    let rupeeUpdateTask = null;
+  function resizeStyleHandler() {
+    document.body.classList.remove('mobile');
+    if (resizeStyleTask != null) window.clearTimeout(resizeStyleTask);
+    resizeStyleTask = window.setTimeout(() => {
+      if (elemNavFirst.offsetTop !== elemNavLast.offsetTop) {
+        document.body.classList.add('mobile');
+      }
+    }, 1);
+  }
 
-    (storedRupeeCount => {
-        if (!isNaN(storedRupeeCount)) {
-            rupeeCount = displayedRupeeCount = Math.max(Math.min(storedRupeeCount, 999), 0);
-        }
-    })(parseInt(window.sessionStorage.getItem('rupees'), 10));
+  resizeHandlers.push(resizeStyleHandler);
 
-    // navbar
-    const elemNav = document.getElementsByTagName('nav')[0];
-    const elemNavHl = document.getElementById('nav-highlight');
-    function genNavHlMutator(elem) {
-        return () => {
-            const parentBounds = elemNav.getBoundingClientRect();
-            const elemBounds = elem.getBoundingClientRect();
-            elemNavHl.style.width = `${elemBounds.width}px`;
-            elemNavHl.style.left = `${elemBounds.left - parentBounds.left}px`;
+  // mobile menu toggle
+  const elemHeader = document.getElementsByTagName('header')[0];
+  document.getElementById('mobile-expand').onclick = () => {
+    elemHeader.classList.toggle('expanded');
+  };
+
+  // rupees
+  const elemRupeeCount = document.getElementById('rupee-counter-text');
+
+  function setDisplayedRupeeCount(count) {
+    elemRupeeCount.innerText = count.toString().padStart(3, '0');
+  }
+
+  setDisplayedRupeeCount(rupeeCount);
+  const rupeeValues = [
+    {value: 1, sound: '1'},
+    {value: 5, sound: '5'},
+    {value: 10, sound: '10'},
+    {value: 20, sound: 'big'},
+    {value: 50, sound: 'big'},
+    {value: 100, sound: 'big'},
+    {value: 200, sound: 'big'},
+  ];
+  const rupeeSounds = ['1', '5', '10', 'big']
+      .reduce((m, s) => {
+        m[s] = document.getElementById(`rupee-audio-${s}`);
+        return m;
+      }, {});
+  const selectRupee = (() => {
+    let segments = rupeeValues.map(r => 1000 / r.value);
+    let accum = 0;
+    segments = segments.map(w => accum += w);
+    return () => {
+      const selection = Math.random() * accum;
+      for (let i = 0; i < segments.length; ++i) {
+        if (selection < segments[i]) return rupeeValues[i];
+      }
+      throw new Error('what the heck?');
+    };
+  })();
+  const sndRupeeChangeProgress = document.getElementById('rupee-audio-change-progress');
+  const sndRupeeChangeDone = document.getElementById('rupee-audio-change-done');
+
+  function updateRupeeCount(offset) {
+    if (rupeeCount < 999) {
+      rupeeCount = Math.min(rupeeCount + offset, 999);
+      window.sessionStorage.setItem('rupees', rupeeCount.toString());
+      if (rupeeUpdateTask === null) {
+        window.setTimeout(function rupeeUpdateTick() {
+          setDisplayedRupeeCount(++displayedRupeeCount);
+          if (displayedRupeeCount !== rupeeCount) {
+            sndRupeeChangeProgress.playRefreshing();
+            rupeeUpdateTask = setTimeout(rupeeUpdateTick, 36);
+          } else {
+            sndRupeeChangeDone.playRefreshing();
+            rupeeUpdateTask = null;
+          }
+        }, 250);
+      }
+    }
+  }
+
+  (rupeeGenBatch => {
+    console.log(2 * document.body.clientHeight / window.innerHeight);
+    let left = Math.random() < 0.5;
+    for (let i = 0; i < rupeeGenBatch; ++i) {
+      (rupee => {
+        const elemRupee = document.createElement('div');
+        const elemRupeeImg = document.createElement('img');
+        elemRupeeImg.setAttribute('src', `/static/img/rupee/${rupee.value}.png`);
+        elemRupeeImg.classList.add('rupee-img');
+        elemRupee.appendChild(elemRupeeImg);
+        elemRupee.classList.add('rupee');
+        elemRupee.onclick = () => {
+          elemRupee.remove();
+          rupeeSounds[rupee.sound].playRefreshing();
+          updateRupeeCount(rupee.value);
         };
+        elemRupee.style.left = (left = !left) ? `${Math.random() * 8}%` : `${Math.random() * 8 + 92}%`;
+        elemRupee.style.top = `${Math.random() * 80 + 10}%`;
+        elemWrapper.appendChild(elemRupee);
+      })(selectRupee());
     }
-    const path = document.location.pathname;
-    const file = path === '/' ? 'index' : path.substring(path.lastIndexOf('/') + 1, path.lastIndexOf('.html'));
-    const elemNavSelected = document.getElementById(`nav-elem-${file}`);
-    const resetNavHl = genNavHlMutator(elemNavSelected);
-    elemNav.onmouseleave = resetNavHl;
-    for (const elem of document.getElementsByClassName('nav-elem')) {
-        elem.onmouseenter = genNavHlMutator(elem);
-    }
-    resizeHandlers.push(resetNavHl);
+  })(0.001 + Math.random() * 2 * document.body.clientHeight / window.innerHeight);
 
-    // mobile stylesheet application
-    const elemNavFirst = elemNav.firstElementChild;
-    const elemNavLast = elemNav.lastElementChild.previousElementSibling;
-    let resizeStyleTask = null;
-    function resizeStyleHandler() {
-        document.body.classList.remove('mobile');
-        if (resizeStyleTask != null) window.clearTimeout(resizeStyleTask);
-        resizeStyleTask = window.setTimeout(() => {
-            if (elemNavFirst.offsetTop !== elemNavLast.offsetTop) {
-                document.body.classList.add('mobile');
-            }
-        }, 1);
-    }
-    resizeHandlers.push(resizeStyleHandler);
-
-    // mobile menu toggle
-    const elemHeader = document.getElementsByTagName('header')[0];
-    document.getElementById('mobile-expand').onclick = () => {
-        elemHeader.classList.toggle('expanded');
-    };
-
-    // rupees
-    const elemRupeeCount = document.getElementById('rupee-counter-text');
-    function setDisplayedRupeeCount(count) {
-        elemRupeeCount.innerText = count.toString().padStart(3, '0');
-    }
-    setDisplayedRupeeCount(rupeeCount);
-    const rupeeValues = [
-        { value: 1, sound: '1' },
-        { value: 5, sound: '5' },
-        { value: 10, sound: '10' },
-        { value: 20, sound: 'big' },
-        { value: 50, sound: 'big' },
-        { value: 100, sound: 'big' },
-        { value: 200, sound: 'big' },
-    ];
-    const rupeeSounds = ['1', '5', '10', 'big']
-        .reduce((m, s) => {
-            m[s] = document.getElementById(`rupee-audio-${s}`);
-            return m;
-        }, {});
-    const selectRupee = (() => {
-        let segments = rupeeValues.map(r => 1000 / r.value);
-        let accum = 0;
-        segments = segments.map(w => accum += w);
-        return () => {
-            const selection = Math.random() * accum;
-            for (let i = 0; i < segments.length; ++i) {
-                if (selection < segments[i]) return rupeeValues[i];
-            }
-            throw new Error('what the heck?');
-        };
-    })();
-    const sndRupeeChangeProgress = document.getElementById('rupee-audio-change-progress');
-    const sndRupeeChangeDone = document.getElementById('rupee-audio-change-done');
-    function updateRupeeCount(offset) {
-        if (rupeeCount < 999) {
-            rupeeCount = Math.min(rupeeCount + offset, 999);
-            window.sessionStorage.setItem('rupees', rupeeCount.toString());
-            if (rupeeUpdateTask === null) {
-                window.setTimeout(function rupeeUpdateTick() {
-                    setDisplayedRupeeCount(++displayedRupeeCount);
-                    if (displayedRupeeCount !== rupeeCount) {
-                        sndRupeeChangeProgress.playRefreshing();
-                        rupeeUpdateTask = setTimeout(rupeeUpdateTick, 36);
-                    } else {
-                        sndRupeeChangeDone.playRefreshing();
-                        rupeeUpdateTask = null;
-                    }
-                }, 250);
-            }
-        }
-    }
-    window.setTimeout(() => (rupeeGenBatch => {
-        console.log(2 * document.body.clientHeight / window.innerHeight);
-        let left = Math.random() < 0.5;
-        for (let i = 0; i < rupeeGenBatch; ++i) {
-            (rupee => {
-                const elemRupee = document.createElement('div');
-                const elemRupeeImg = document.createElement('img');
-                elemRupeeImg.setAttribute('src', `/static/img/rupee/${rupee.value}.png`);
-                elemRupeeImg.classList.add('rupee-img');
-                elemRupee.appendChild(elemRupeeImg);
-                elemRupee.classList.add('rupee');
-                elemRupee.onclick = () => {
-                    elemRupee.remove();
-                    rupeeSounds[rupee.sound].playRefreshing();
-                    updateRupeeCount(rupee.value);
-                };
-                elemRupee.style.left = (left = !left) ? `${Math.random() * 8}%` : `${Math.random() * 8 + 92}%`;
-                elemRupee.style.top = `${Math.random() * 80 + 10}%`;
-                elemWrapper.appendChild(elemRupee);
-            })(selectRupee());
-        }
-    })(0.001 + Math.random() * 2 * document.body.clientHeight / window.innerHeight), 10); // FIXME race condition
-
-    // init
-    window.onresize = () => {
-        for (const handler of resizeHandlers) handler();
-    };
-    elemMain.style.display = 'block';
+  // init
+  window.onresize = () => {
+    for (const handler of resizeHandlers) handler();
+  };
+  elemMain.style.display = 'block';
+  if (document.fonts && document.fonts.ready) {
     document.fonts.ready.then(() => {
-        resetNavHl();
-        resizeStyleHandler();
-        window.setTimeout(() => document.body.classList.add('loaded'), 10); // FIXME race condition
+      resetNavHl();
+      resizeStyleHandler();
+      document.body.classList.add('loaded');
     });
+  } else {
+    window.setTimeout(() => {
+      resetNavHl();
+      resizeStyleHandler();
+      document.body.classList.add('loaded')
+    }, 50);
+  }
 
-})();
+};
